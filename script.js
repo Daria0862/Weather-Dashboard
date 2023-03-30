6d34b0af001d42933ae1d0d1b16b706630bde58b
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-30 22:57:29 +0100

    compelted end

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var APIKey = "e2155e349b6d5e6dd0461111d10358aa";
+var apiKey = "e2155e349b6d5e6dd0461111d10358aa";
 var city;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
@@ -9,51 +9,64 @@ var todaySection = document.querySelector('#today');
 var forecastSection = document.querySelector('#forecast');
 
 function getWeather(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-  
-    fetch(queryURL)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-  
-        displayWeather(data);
-      })
-      .catch(function(error) {
-        console.log(error);
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+  fetch(queryURL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+      displayWeather(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function displayWeather(data) {
+  var todayHTML = `
+    <div class="card mb-3">
+      <div class="card-body">
+        <h2 class="card-title">${data.name} (${moment().format('D/MM/YYYY')})</h2>
+        <p class="card-text">Temperature: ${data.main.temp} &deg;F</p>
+        <p class="card-text">Humidity: ${data.main.humidity}%</p>
+        <p class="card-text">Wind Speed: ${data.wind.speed} MPH</p>
+      </div>
+    </div>
+  `;
+  todaySection.innerHTML = todayHTML;
+
+  var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+  fetch(forecastURL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var forecastData = data.list.filter(function(item) {
+        var itemDate = moment(item.dt_txt).format("YYYY-MM-DD");
+        var next5Days = moment().add(5, "days").format("YYYY-MM-DD");
+        return itemDate <= next5Days;
       });
-  }
-    function displayWeather(data) {
-        var todayHTML = `
-          <div class="card mb-3">
-            <div class="card-body">
-              <h2 class="card-title">${data.name} (${moment().format('D/MMYYYY')})</h2>
-              <p class="card-text">Temperature: ${data.main.temp} &deg;F</p>
-              <p class="card-text">Humidity: ${data.main.humidity}%</p>
-              <p class="card-text">Wind Speed: ${data.wind.speed} MPH</p>
-            </div>
-          </div>
-        `;
-        todaySection.innerHTML = todayHTML;
-      
-        var forecastHTML = '';
-        for (var i = 1; i <= 5; i++) {
-          var forecastData = data.daily[i];
-          forecastHTML += `
-            <div class="col-md">
-              <div class="card bg-primary text-white">
-                <div class="card-body">
-                  <h5 class="card-title">${moment.unix(forecastData.dt).format('M/D/YYYY')}</h5>
-                  <img src="https://openweathermap.org/img/w/${forecastData.weather[0].icon}.png" alt="${forecastData.weather[0].description}" />
-                  <p class="card-text">Temp: ${forecastData.temp.day} &deg;F</p>
-                  <p class="card-text">Humidity: ${forecastData.humidity}%</p>
-                </div>
-              </div>
-            </div>
-          `;
-        }
-        forecastSection.innerHTML = forecastHTML;
-      }
-      
-     
+      displayForecast(forecastData);
+    })
+    .catch(function(error) {
+      console.log("Error fetching forecast data:", error);
+    });
+}
+
+function displayForecast(data) {
+
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var cityName = searchInput.value.trim();
+
+  getWeather(cityName);
+}
+
+searchButton.addEventListener('click', handleFormSubmit);
+
