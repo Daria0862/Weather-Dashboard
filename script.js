43d1d3c949e2a7d20c016df4d3c2e497754fc1d7
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-30 22:47:42 +0100

    starter and declare vars

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var APIKey = "e2155e349b6d5e6dd0461111d10358aa";
+var city;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
-var city = prompt("Enter city name:");
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-
-fetch(queryURL);
+var searchButton = document.querySelector('.search-button');
+var searchInput = document.querySelector('#search-input');
+var historyList = document.querySelector('#history');
+var todaySection = document.querySelector('#today');
+var forecastSection = document.querySelector('#forecast');
